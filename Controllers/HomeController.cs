da282b9fb7ff43b4c22d19faf09c480a6a80b84b
Switchblade4703 <156360989+Switchblade4703@users.noreply.github.com>
2024-09-08 18:00:07 -0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using _2_1.Models;
+
+namespace _2_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult index()
+        {
+            ViewBag.FV = 0;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(FutureValueModel model)
+        {
+            ViewBag.FV = model.CalculateFutureValue();
+            return View(model);
+        }
+    }
+}
